d1faa91ef4281107a2b1a6744c1f947252dbe39f
Viet Hai Trinh <satellarknighty@gmail.com>
2023-02-17 11:09:31 +0100

    Initial commit Solitaire project

---
+++ Solitaire/src/main/java/de/cxi/solitaire/SolitaireApplication.java
@@ -0,0 +1,13 @@
+package de.cxi.solitaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolitaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolitaireApplication.class, args);
+    }
+
+}
---
+++ Solitaire/src/test/java/de/cxi/solitaire/SolitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cxi.solitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolitaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
