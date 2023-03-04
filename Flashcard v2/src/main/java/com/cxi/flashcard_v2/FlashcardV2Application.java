4f8d234dc764b02888b12155f219c0372109a2c1
Viet Hai Trinh <satellarknighty@gmail.com>
2023-03-04 22:40:03 +0100

    Initial commit

---
+++ Flashcard v2/src/main/java/com/cxi/flashcard_v2/FlashcardV2Application.java
@@ -0,0 +1,13 @@
+package com.cxi.flashcard_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardV2Application.class, args);
+    }
+
+}
---
+++ Flashcard v2/src/test/java/com/cxi/flashcard_v2/FlashcardV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxi.flashcard_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
