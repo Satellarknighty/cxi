b137b0be40e0f54903c145e3366407d3874c164c
Hai Trinh <satellarknighty@gmail.com>
2024-03-27 15:53:42 +0100

    using path variable for finding the course that the exercises belong to

--- uni-notes/src/main/java/com/cxi/uninotes/controllers/ExerciseController.java
+++ uni-notes/src/main/java/com/cxi/uninotes/controllers/ExerciseController.java
@@ -6,12 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("exercise")
+@RequestMapping("/exercise/{name}")
 @RequiredArgsConstructor
 public class ExerciseController {
     private final ExerciseService exerciseService;
     @PostMapping
-    public String addExerciseToACourse(@RequestParam String courseName,
+    public String addExerciseToACourse(@PathVariable("name") String courseName,
                                        @RequestBody Exercise exercise){
         exerciseService.addExerciseToACourse(courseName, exercise);
         return String.format("Exercise number %d has been successfully added to course %s!",
