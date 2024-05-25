07925f1536c3240991f9a9024a463e81d900ca70
Viet Hai Trinh <satellarknighty@gmail.com>
2024-05-25 10:45:18 +0200

    JavaScript basics

--- mdn-tutorial/budapest-gambit/scripts/main.js
+++ mdn-tutorial/budapest-gambit/scripts/main.js
@@ -1,4 +1,41 @@
-document.querySelector("h1").textContent = "Hello World!";
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ouch! Stop poking me!");
-  });
+const img = document.querySelector("img");
+
+img.onclick = () => {
+  const imgSrc = img.getAttribute("src");
+  if (imgSrc === "images/budapest-gambit.png") {
+    img.setAttribute("src", "images/dxe5.png");
+  }
+  else {
+    img.setAttribute("src", "images/budapest-gambit.png");
+  }
+
+  let button = document.querySelector("button");
+  let heading1 = document.querySelector("h1");
+
+  function checkValidName(name){
+    return name !== null && name !== "";
+  }
+
+  function setUserName() {
+    const userName = prompt("Please enter your name:");
+    if (!checkValidName(userName)){
+
+    }
+    else{
+      localStorage.setItem("name", userName);
+      heading1.textContent = `Learn the Budapest Gambit, ${userName}.`;
+    }
+  }
+
+  if (!checkValidName(localStorage.getItem("name"))){
+    setUserName();
+  }
+  else {
+    const storedName = localStorage.getItem("name");
+    heading1.textContent = `Learn the Budapest Gambit, ${storedName}.`;
+  }
+
+  button.onclick = () => {
+    setUserName();
+  }
+}
