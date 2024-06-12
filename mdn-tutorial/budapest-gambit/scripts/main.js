10c1c1b5ed64aad3f59631c299bc11f2db87e369
Hai Trinh <satellarknighty@gmail.com>
2024-06-12 23:18:30 +0200

    pseudo classes and elements

--- mdn-tutorial/budapest-gambit/scripts/main.js
+++ mdn-tutorial/budapest-gambit/scripts/main.js
@@ -8,34 +8,35 @@ img.onclick = () => {
   else {
     img.setAttribute("src", "images/budapest-gambit.png");
   }
+}
 
-  let button = document.querySelector("button");
-  let heading1 = document.querySelector("h1");
+let button = document.querySelector("button");
+let heading1 = document.querySelector("h1");
 
-  function checkValidName(name){
-    return name !== null && name !== "";
+function checkValidName(name){
+  return name !== null && name !== "";
+}
+
+function setUserName() {
+  const userName = prompt("Please enter your name:");
+  if (!checkValidName(userName)){
+
   }
-
-  function setUserName() {
-    const userName = prompt("Please enter your name:");
-    if (!checkValidName(userName)){
-
-    }
-    else{
-      localStorage.setItem("name", userName);
-      heading1.textContent = `Learn the Budapest Gambit, ${userName}.`;
-    }
+  else
+  {
+    localStorage.setItem("name", userName);
+    heading1.textContent = `Learn the Budapest Gambit, ${userName}.`;
   }
+}
 
-  if (!checkValidName(localStorage.getItem("name"))){
-    setUserName();
-  }
-  else {
-    const storedName = localStorage.getItem("name");
-    heading1.textContent = `Learn the Budapest Gambit, ${storedName}.`;
-  }
+if (!checkValidName(localStorage.getItem("name"))){
+  setUserName();
+}
+else {
+  const storedName = localStorage.getItem("name");
+  heading1.textContent = `Learn the Budapest Gambit, ${storedName}.`;
+}
 
-  button.onclick = () => {
-    setUserName();
-  }
+button.onclick = () => {
+  setUserName();
 }
